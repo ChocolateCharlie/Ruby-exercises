51b4ad404b93b6cdac515d6e04471d23bd0d5643
ChocolateCharlie <aurore.amrit@gmail.com>
2018-06-30 11:33:08 +0200

    Create 'Take your first steps with Ruby' and finish first part

---
+++ Openclassrooms/Take-your-first-steps-with-Ruby/3-Create-a-person-with-a-name-age-and-outfit/person.rb
@@ -0,0 +1,11 @@
+class Person
+	attr_accessor :name, :age, :outfit
+end
+
+
+
+person_1 = Person.new
+
+person_1.name = "Arnaud"
+person_1.age = 32
+person_1.outfit = ["blue top", "red pants", "white shoes"]
