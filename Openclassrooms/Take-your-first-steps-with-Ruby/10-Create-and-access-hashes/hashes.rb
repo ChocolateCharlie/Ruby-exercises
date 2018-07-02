8cd00962667cdf318e29cac21007a4075939552f
ChocolateCharlie <aurore.amrit@gmail.com>
2018-07-02 13:04:12 +0200

    Finish 'Take your first steps with Ruby'

---
+++ Openclassrooms/Take-your-first-steps-with-Ruby/10-Create-and-access-hashes/hashes.rb
@@ -0,0 +1,18 @@
+favorites = {
+	books: ["Paris guidebook", "The Art of Innovation"],
+	fruits: ["apples", "oranges", "pears"],
+	colors: ["yellow", "green"]
+}
+
+state_capitals = {
+	new_york: "Albany",
+	pennsylvania: "Harrisburg",
+	colorado: "Denver",
+	maine: "Augusta",
+	vermont: "Montpelier"
+}
+
+
+
+print( state_capitals[:new_york] )
+print( favorites[:books] )
