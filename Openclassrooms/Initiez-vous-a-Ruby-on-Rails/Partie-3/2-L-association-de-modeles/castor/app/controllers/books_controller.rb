7bafe8c0d2481cb72661381b68c7488dcb9a8dde
ChocolateCharlie <aurore.amrit@gmail.com>
2018-07-09 22:02:46 +0200

    Finish 'L\'association de modèles'

---
+++ Openclassrooms/Initiez-vous-a-Ruby-on-Rails/Partie-3/2-L-association-de-modeles/castor/app/controllers/books_controller.rb
@@ -0,0 +1,26 @@
+class BooksController < ApplicationController
+  def index
+    @books = Book.all
+  end
+
+  def create
+    book = Book.new
+    book.title = params[:title]
+    book.save
+    redirect_to "/books"
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    Book.find(params[:id]).update title: params[:title]
+    redirect_to "/books/#{params[:id]}"
+  end
+
+  def destroy
+    Book.find(params[:id]).destroy
+    redirect_to "/books"
+  end
+end
