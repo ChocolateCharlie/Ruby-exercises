b9ad961bf52205a4c2e5677cfa6a79eb7e425847
ChocolateCharlie <aurore.amrit@gmail.com>
2018-07-15 11:31:22 +0200

    Finish 'Maintenez la connexion avec les sessions'

---
+++ Openclassrooms/Continuez-avec-Ruby-on-Rails/Partie-2/2-Maintenez-la-connexion-avec-les-sessions/partie2/app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+  def home
+    if session[:user_id]
+      @current_user = User.find(session[:user_id])
+    end
+  end
+
+  def login
+  end
+
+  def check
+    @current_user = User.where(name: params[:name], password: params[:password]).first
+    if @current_user
+      session[:user_id] = @current_user.id
+      flash[:info] = "Bienvenue #{@current_user.name} !"
+      redirect_to "/users/home"
+    else
+      session[:user_id] = nil
+      flash[:info] = "Echec de la connexion"
+      redirect_to "/users/login"
+    end
+  end
+
+  def logout
+    session[:user_id] = nil
+    flash[:info] = "Déconnexion réussie"
+    redirect_to "/users/home"
+  end
+end
